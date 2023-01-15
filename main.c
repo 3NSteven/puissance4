4676607a2c02d69a3adae02e97802682ea3e1c87
3NSteven <pereira.steven36@gmail.com>
2023-01-15 18:39:25 +0100

    second commit

--- main.c
+++ main.c
@@ -2,30 +2,39 @@
 #include <string.h>
 #include <stdlib.h>
 
-void afficheGrille(char grille[6][7]){
-    for (int i = 0; i < 6; i++)
+#include <stdbool.h>
+
+#include "settings.h"
+
+/*
+    Affiche la grille de jeu
+*/
+void afficheGrille(char grille[MAX_LINE][MAX_COLONNE]){
+
+    for (int i = 0; i < MAX_LINE; i++)
     {
-        printf("|");
-        for (int j = 0; j < 7; j++)
-        {
-            printf(" ");
-           putchar(grille[i][j]);
-           printf(" |");
-        }
         printf("\n");
+        for (int j = 0; j < MAX_COLONNE; j++)
+        {
+            printf("| %c ", grille[i][j]);
+        }
+        printf("|");
     }
+    printf("\n\n");
+
 }
 
-int creerGrille(char (*grille2)[7]){
-    
-    for (int i = 0; i < 6; i++)
+/*
+ Créer une grille 6x7 vide
+*/
+void creerGrille(char (*grille2)[MAX_COLONNE]){
+    for (int i = 0; i < MAX_LINE; i++)
     {
-        for (int j = 0; j < 7; j++)
+        for (int j = 0; j < MAX_COLONNE; j++)
         {
             *(*(grille2 + i) + j) = ' ';
         }
     }
-
 }
 
 /*
@@ -33,7 +42,7 @@ int creerGrille(char (*grille2)[7]){
 
     Retourne 0 ou un code d'erreur
 */
-int ajouterJeton(char (*grille2)[7], int colonne){
+int ajouterJeton(char (*grille2)[MAX_COLONNE], int colonne){
 
 
     for (int i = 5; i >= 0; i--)
@@ -42,10 +51,12 @@ int ajouterJeton(char (*grille2)[7], int colonne){
         if( (*(*(grille2+i)+colonne)) == ' ')
         {
             *(*(grille2 + i) + colonne) = 'X';
+            afficheGrille(grille2); 
             return 0;
         }
         else if( i == 0 ){
-            printf("This column is full ! Choose another one.\n");
+            afficheGrille(grille2); 
+            printf("Cette colonne est plein, selectionné une autre colonne:\n\n");
             return -1;
         }
 
@@ -53,59 +64,90 @@ int ajouterJeton(char (*grille2)[7], int colonne){
 
 }
 
-int partie(char * joueur1, char * joueur2, int tour){
+int partie(char *J1, char *J2){
 
-    //initialise la grille
-    creerGrille(grille);
+    int fin = 0; //definie la fin d'une partie
+    int tour = 0; //definie le nombre de tour joué
+    while (!fin)
+    {
+       if (tour%2 == 0) // Tour du Joueur1
+       {
+           
+       }
+       else // Tour du Joueur2
+       {
 
-    printf("\n--------ROUND %d START--------\n", i+1);
+       }
 
-    while(0){   //tant que personne n'a aligne quatre jetons
-        afficheGrille(grille);
+       tour++;
+    }
 
-        if(tour%2 != 0){    //tour impair (joueur1)
-            printf("Au tour de %s", joueur1);
-        }
-        else if(tour%2 == 0){   //tour pair (joueur2)
-            printf("Au tour de %s", joueur2);
+}
+
+int fixesettings(char *J1, char *J2, joueur* J){
+
+    bool findIA = false;
+    for (int i = 0; i < 2; i++)
+    {
+        if ((strcmp(J1, IA[i]) == 0) || (strcmp(J2, IA[i]) == 0))
+        { 
+           findIA = true; 
         }
     }
 
+    if(!findIA){
+        printf("Il n'y a aucun nom d'IA parmis les noms des joueurs.\n");
+        printf("Ex: './main Charles IA0' ou encore './main IA1 Pablo'\n");
+        return -1;
+    }
+    else{
+
         
-    printf("--------ROUND %d OVER---------\n\n", i+1);
+
+    }
 
 }
+
+
 
 int main(int argc, char * argv[]){
 
     int nbparties = 0;
-
+    
     if(argc == 3) nbparties = 1;
     else if(argc == 4) nbparties = atoi(argv[3]);
     else {
-        printf("Use:\n");
-        printf("%s Player1 Player2 Rounds \n", argv[0]);
+        printf("Insctruction de lancement\n");
+        printf("%s Joueur1 Joueur2 (nbpartie) \n", argv[0]);
         return 1;
     }
 
-    printf("Player 1 : %s\n", argv[1]);
-    printf("Player 2 : %s\n", argv[2]);
-    printf("Number of Rounds: %d\n", nbparties);
+    printf("Joueur 1 : %s\n", argv[1]);
+    printf("Joueur 2 : %s\n", argv[2]);
+    printf("Nombre de partie à lancer : %d\n", nbparties);
 
-    char * joueur1 = argv[1];
-    char * joueur2 = argv[2];
-
-
+    // Initialise la gille
     char grille[6][7];
+
+    creerGrille(grille);
+    afficheGrille(grille);
+
+    // fixé les parametres des joueurs
+    joueur* joueurs = malloc( 2 * sizeof *joueurs);
     
+    //strcpy(joueurs[i].Jnom, Nomjoueur[i]);
 
-    for(int tour = 1; tour<=nbparties; tour++){   //chaque partie
-        
-        partie(joueur1, joueur2, tour);
+    fixesettings(argv[1], argv[2], joueurs);
 
+    /*
+    for (int i = 0; i < 7; i++)
+    {
+        ajouterJeton(grille, 3);
+    }
+    */
 
+    for(int i = 0; i < nbparties; i++){
+        //partie(, ); 
     }
 
-    
-    
 }
---
+++ settings.h
@@ -0,0 +1,32 @@
+#ifndef SETTINGS
+#define SETTINGS
+
+#define MAX_LINE 6
+#define MAX_COLONNE 7
+
+#include <string.h>
+
+#include <stdbool.h>
+
+void afficheGrille(char grille[MAX_LINE][MAX_COLONNE]);
+void creerGrille(char (*grille2)[MAX_COLONNE]);
+int ajouterJeton(char (*grille2)[MAX_COLONNE], int colonne);
+
+
+const char * IA[] = {
+    "IA0",
+    "IA1"
+};
+
+typedef struct joueur
+{
+    char *Jnom; // Le nom du joueur
+    char Jsign; // Quel jeton il posséde
+    bool isIA; // Si c'est une IA
+    int partieGagner;
+}joueur;
+
+
+int partie(char *J1, char *J2);
+
+#endif
